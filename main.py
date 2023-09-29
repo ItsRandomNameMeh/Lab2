4a5ac4906f9b258ff067dfa8418be629f5cba32a
ItsRandomNameMeh <d.enotow@yandex.ru>
2023-09-29 18:59:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+class Hero():
+    __numb = 0 #номер объекта
+    __team = 0 #свойство принадлежности к команде
+    __damage = 0
+    __speed = 0
+    def __init__(self, numb, team):
+        pass
+
+    def __str__(self):
+        return f"Hero: {self.numb}, {self.team}, {self.damage}, {self.speed}"
+    @property
+    def numb(self):
+        return self.numb
+    @property
+    def team(self):
+        return self.team
+    @property
+    def damage(self):
+        return self.damage
+    @property
+    def speed(self):
+        return self.speed
+
+
+class Wizard(Hero):
+    __lvl = 0
+    @property
+    def lvl(self):#уровень мага. по условию этот свойсто есть только у мага
+        return self.lvl
+
+class Paladin(Hero):
+    __follow = 0
+    @property
+    def follow(self):#свойство следования, зависит от номера мага, за которым будет следовать
+        return self.follow
